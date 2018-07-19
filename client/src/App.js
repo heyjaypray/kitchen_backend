0bd2acb46e6822fe42e11cf9eeeaebeae7664dea
Justin Romanos <hey@jaypray.com>
2018-07-19 18:57:02 -0400

    a

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Jumbo from './components/jumbo';
 import Photos from './components/photos';
 import Blog from './components/blog';
 import Welcome from './components/welcome';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
